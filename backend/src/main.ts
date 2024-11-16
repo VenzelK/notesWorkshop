fe213496f9cdf391ac32cd918a030315dc4531a6
VenzelK <93466898+VenzelK@users.noreply.github.com>
2024-11-17 01:26:40 +0700

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 4000);
+  const config = new DocumentBuilder()
+    .setTitle('NotesApi')
+    .setDescription('Api description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'JWT',
+    )
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
