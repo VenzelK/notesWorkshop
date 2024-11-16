f22eda9017199b3a8953cd938c40accfd71e0b18
VenzelK <93466898+VenzelK@users.noreply.github.com>
2024-11-17 01:32:56 +0700

    add mail module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 import { NotesModule } from './notes/notes.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [NotesModule, UserModule, AuthModule],
+  imports: [NotesModule, UserModule, AuthModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/mail/mail.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AccessTokenGuard } from 'src/auth/guards/accessToken.guard';
+import { MailService } from './mail.service';
+
+@Controller('mail')
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
+
+  @Post('/post')
+  @ApiBearerAuth('JWT')
+  @UseGuards(AccessTokenGuard)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  postMailMessage(@Request() req) {
+    return this.mailService.postMail(+req.user['sub'], req.user['email']);
+  }
+}
