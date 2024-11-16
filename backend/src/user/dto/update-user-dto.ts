6a0f4f743533089dc9f5353948d41bb09871611f
VenzelK <93466898+VenzelK@users.noreply.github.com>
2024-11-17 02:06:15 +0700

    fix prisma scheme

--- backend/src/user/dto/update-user-dto.ts
+++ backend/src/user/dto/update-user-dto.ts
@@ -1,13 +1,12 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsJWT, IsString, MaxLength } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiPropertyOptional()
-  @IsJWT()
-  refreshToken?: string;
+  refreshToken?: string | null;
 
   @ApiPropertyOptional()
-  @MaxLength(4)
-  @IsString()
-  emailCode?: string;
+  emailVerificationCode?: string | null;
+
+  @ApiPropertyOptional()
+  emailVerification?: boolean;
 }
