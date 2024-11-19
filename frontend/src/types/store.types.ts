88a2e025831fe0ba56ca6c22c43c2e90dc46af23
VenzelK <93466898+VenzelK@users.noreply.github.com>
2024-11-20 02:32:02 +0700

    add AccessToken type

--- frontend/src/types/store.types.ts
+++ frontend/src/types/store.types.ts
@@ -1,6 +1,14 @@
+import { JwtPayload } from 'jwt-decode'
+
 export interface StoreEndpoints {
 	login: string
 	register: string
 	mailPost: string
 	mailCheck: string
+}
+
+export interface AccessToken extends JwtPayload {
+	emailVerification: boolean
+	email: string
+	role: string
 }
