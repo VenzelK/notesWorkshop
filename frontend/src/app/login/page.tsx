83f549159ff92d51012a9ee14154f210e07a5690
VenzelK <93466898+VenzelK@users.noreply.github.com>
2024-11-19 23:52:07 +0700

    add login page

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,22 @@
+import LoginForm from '@/components/form/LoginForm'
+import Icon from '@/components/icon/Icon'
+import { NextPage } from 'next'
+
+const page: NextPage = () => {
+	return (
+		<div className='flex flex-row gap-10 h-screen justify-center items-center m-auto border-4  px-6'>
+			<div className='flex flex-row justify-between bg-menu_color w-4/12 rounded-menu_radius bg-opacity-5 pl-8 pr-4 h-[420px] items-center'>
+				<div className='font-bold text-text_color text-[24px] text-wrap max-w-36 '>
+					Вход в Мастерскую заметок
+				</div>
+				<div className=''>
+					<Icon type='logo' size={182} className='text-logo_color' />
+				</div>
+			</div>
+
+			<LoginForm className='bg-menu_color flex-1 rounded-menu_radius flex flex-col justify-between pt-28' />
+		</div>
+	)
+}
+
+export default page
---
+++ frontend/src/components/form/LoginForm.tsx
@@ -0,0 +1,147 @@
+'use client'
+
+import useLogin from '@/hooks/query/useLogin'
+import useRegister from '@/hooks/query/useRegister'
+import { UnifiedResponse } from '@/types/login.types'
+import { useIsFetching, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
+import Input from '../input/Input'
+import styles from './LoginForm.module.css'
+
+import { jwtDecode } from 'jwt-decode'
+
+import { useCookies } from 'next-client-cookies'
+
+interface LoginFormProps {
+	className?: string
+}
+
+type LoginFormInputs = {
+	mail: string
+	password: string
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ className }) => {
+	const [passVisible, setPassVisible] = useState(false)
+
+	const isFetching = useIsFetching({ queryKey: ['login'] })
+
+	const router = useRouter()
+
+	const cookies = useCookies()
+
+	const queryClient = useQueryClient()
+
+	const { mutate: loginMutate } = useLogin()
+	const { mutate: registerMutate } = useRegister()
+
+	const onSuccess = (response: {
+		errorField?: 'mail' | 'password'
+		payload?: UnifiedResponse
+	}) => {
+		if (response.errorField) {
+			setError(response.errorField, { type: 'value' })
+
+			return
+		}
+		clearErrors()
+
+		if (response.payload?.accessToken && response.payload?.refreshToken) {
+			queryClient.setQueryData(['login'], {
+				...response.payload,
+				...jwtDecode(response.payload.accessToken),
+			})
+
+			cookies.set('accessToken', response.payload?.accessToken, {
+				path: '/',
+			})
+			cookies.set('refreshToken', response.payload?.refreshToken, { path: '/' })
+		}
+
+		router.push('/login/mailCode/')
+	}
+
+	const {
+		register,
+		handleSubmit,
+		setError,
+		clearErrors,
+		formState: { errors },
+	} = useForm<LoginFormInputs>()
+
+	const onSubmit: SubmitHandler<LoginFormInputs> = (data, event) => {
+		const eventType = (event?.nativeEvent as SubmitEvent).submitter?.id
+
+		if (eventType == 'registerBtn') {
+			registerMutate(
+				{ mail: data.mail, password: data.password },
+				{ onSuccess }
+			)
+		} else {
+			loginMutate({ mail: data.mail, password: data.password }, { onSuccess })
+		}
+	}
+
+	return (
+		<form className={className} onSubmit={handleSubmit(onSubmit)}>
+			<div className={styles.container}>
+				<div className={styles.clue_container}>
+					<label className={styles.clue}>Почта</label>
+					<label className={styles.clue}>Пароль</label>
+				</div>
+				<div className={styles.input_container}>
+					<Input
+						{...register('mail', { required: true })}
+						type='text'
+						maxLength={20}
+						className={errors.mail && ' border-red'}
+					/>
+
+					<div className='relative'>
+						<Input
+							{...register('password', { required: true })}
+							type={passVisible ? 'text' : 'password'}
+							maxLength={20}
+							className={`${styles.pass_input} + " " + ${
+								errors.password && ' border-red'
+							}`}
+						/>
+
+						<div className={styles.password_eye_container}>
+							<button
+								type='button'
+								className={styles.eye_btn}
+								onClick={() => setPassVisible(!passVisible)}
+							>
+								{passVisible ? <FaRegEye /> : <FaRegEyeSlash />}
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={styles.btn_container}>
+				<button
+					className={styles.send_btn + ' ' + styles.registration_btn}
+					type='submit'
+					disabled={isFetching ? true : false}
+					id='registerBtn'
+				>
+					Зарегистрироваться
+				</button>
+				<button
+					className={styles.send_btn + ' ' + styles.login_btn}
+					type='submit'
+					disabled={isFetching ? true : false}
+					id='loginBtn'
+				>
+					Войти
+				</button>
+			</div>
+		</form>
+	)
+}
+
+export default LoginForm
